c723fea3b28a9265d6cf460f78406a7e5c88b25e
Dima <dmytro.tarasovskyi@vimmi.net>
2020-09-19 22:18:16 +0300

    added navigationController class

---
+++ StylizerApp/UI/ViewControllers/common/NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationController.swift
+//  StylizerApp
+//
+//  Created by Dima on 19.09.2020.
+//  Copyright © 2020 Dima. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController{
+    
+}
