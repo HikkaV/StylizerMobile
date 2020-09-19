3cb6b294749d8be5af2549a025cfa80a27fedf34
Dima <dmytro.tarasovskyi@vimmi.net>
2020-09-19 14:52:07 +0300

    added iterator

--- StylizerApp/Logic/Iterators/Iterator.swift
+++ StylizerApp/Logic/Iterators/Iterator.swift
@@ -7,7 +7,20 @@
 //
 
 import Foundation
+import UIKit
 
 public class Iterator{
+    
+    func permormTransormationRequest(originalImage: UIImage, styleImage: UIImage, completion :  @escaping (Data?, Error?) -> Void){
+        if let data = ImageManager.shared.createDataWithImages(originalImage: originalImage, styleImage: styleImage){
+            RequestService.shared.makeRequest(data: data, url: Constants.serverAdress) { (data, err) in
+                completion(data, err)
+            }
+        }else{
+            completion(nil, nil)
+        }
+       
+        
+    }
     
 }
