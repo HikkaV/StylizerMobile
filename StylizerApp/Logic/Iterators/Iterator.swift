1d7d2077e8154fd6b6bf7d26f714ac72d0e8b7ef
Dima <dmytro.tarasovskyi@vimmi.net>
2020-10-11 17:53:12 +0300

    image loading and configuring was given away on other thread

--- StylizerApp/Logic/Iterators/Iterator.swift
+++ StylizerApp/Logic/Iterators/Iterator.swift
@@ -11,20 +11,22 @@ import UIKit
 
 public class Iterator{
     
+    let queue = DispatchQueue.global(qos: .userInteractive)
+    
     func permormTransormationRequest(originalImage: UIImage, styleImage: UIImage, completion :  @escaping (Data?) -> Void){
-        if let data = ImageManager.shared.createDataWithImages(originalImage: originalImage, styleImage: styleImage){
-            RequestService.shared.makeRequest(data: data, url: Constants.serverAdress) { (data, _) in
-                if let data = data{
-                    completion(data)
-                }else{
-                    completion(nil)
+        queue.async {
+            if let data = ImageManager.shared.createDataWithImages(originalImage: originalImage, styleImage: styleImage){
+                RequestService.shared.makeRequest(data: data, url: Constants.serverAdress) { (data, _) in
+                    if let data = data{
+                        completion(data)
+                    }else{
+                        completion(nil)
+                    }
                 }
+            }else{
+                completion(nil)
             }
-        }else{
-            completion(nil)
         }
-       
-        
     }
     
 }
