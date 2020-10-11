984dd0972046d40a19416ea405f70fd6c732b182
Dima <dmytro.tarasovskyi@vimmi.net>
2020-10-11 17:50:56 +0300

    added static server link (pls do not dos it)

--- StylizerApp/Logic/Common/Constants.swift
+++ StylizerApp/Logic/Common/Constants.swift
@@ -11,9 +11,8 @@ import Foundation
 // replace with R.swift
 
 public struct Constants{
-    static let serverAdress: String = "http://f7d06ad961d7.eu.ngrok.io/stylize"
+    static let serverAdress: String = "https://stylizer-292110.ew.r.appspot.com/stylize"
     static let stringImage: String = "image"
     static let keyOriginalImage: String = "content_image"
     static let keyStyleImage: String = "style_image"
-    
 }
